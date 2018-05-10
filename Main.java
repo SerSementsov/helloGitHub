014bc980f5fba90068576b0a13fe75b3787311d3
ssementsov <>
2018-05-10 10:50:38 +0400

    another commit in master

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("222");
         System.out.println("222");
         System.out.println("new line from master");
+        System.out.println("another line from master");
     }
 
 }
